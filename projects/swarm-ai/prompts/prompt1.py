92a9a3e9d22efd4520c91eb5a21e1359b6f6fdf4
Kenneth Lo, PMP <vadeveloper@gmail.com>
2024-02-22 14:22:17 -0800

    add diffs

--- projects/swarm-ai/prompts/prompt1.py
+++ projects/swarm-ai/prompts/prompt1.py
@@ -1,20 +1,21 @@
-lang = 'golang'
 
 prompt = f"""
 
-You're a {lang} sofware developer. You will be given {error_log}. Inspect the errors, revise the written code to ensure the unit tests given in the triple backticks passed. 
+The code didn't compile. You will be given the error log. Inspect the errors, revise the written code to ensure the unit tests given in the triple backticks passed. 
 
 Perform the following actions:
 
 1. Summarize what the code is intended to do given the tests.
 2. List the names of the functions that these tests are covering.
 3. Provide the code.
-4. Output a JSON object that containts the following keys: test_summary, function_names, code.
+4. Highlight diffs.
+5. Output a JSON object that containts the following keys: test_summary, function_names, code, diffs.
+
 
 Separate your answers in line breaks.
 
 
-tests:
-```{unittests}```
+error log:
+```{error_log}```
 
 """
